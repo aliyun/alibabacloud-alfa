36a0df58b14dad4e25e3894aa62f506c012461d7
Boelroy <liusi.xbw@alibaba-inc.com>
2020-04-29 13:39:46 +0800

    :bug: fix bug for ng mount dom error

--- packages/ng-portal/src/index.ts
+++ packages/ng-portal/src/index.ts
@@ -26,7 +26,6 @@ const unbindEvents = (emitter: EventEmitter) => {
 const getProps = (props) => {
   const appProps = { ...props, ...(props.appProps || {}) };
 
-  delete appProps.domElement;
   delete appProps.singleSpa;
   delete appProps.mountParcel;
 
