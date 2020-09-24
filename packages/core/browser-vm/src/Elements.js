7eca24fdd0c1c83600005fffc3871048459e39ba
liusi.xbw <liusi.xbw@alibaba-inc.com>
2020-09-24 15:09:47 +0800

    :bug: fix bug for ie

--- packages/core/browser-vm/src/Elements.js
+++ packages/core/browser-vm/src/Elements.js
@@ -7,22 +7,25 @@
 
 import injectScriptCallBack from './utils/injectScriptCallBack';
 
+const makeElInjector = (originMethod) => function( el, ...args ){
+  if( el && el.nodeName === 'SCRIPT' && el.ownerAppWindow ){
+    injectScriptCallBack( el );
+  }
+
+  // fix: babel 会把 fn.call 转义成 fn.call.apply 造成 chrome 50 的几个版本报错
+  // 这里 尝试 try catch 如果报错 直接调用 fn.apply
+  try {
+    return originMethod.call( this, el, ...args );
+  } catch {
+    return originMethod.apply( this, [el, ...args] );
+  }
+}
+
 if( typeof window.Element === 'function' ){
   const mountElementMethods = [ 'appendChild', 'insertBefore', 'append' ];
 
   for ( const method of mountElementMethods ) {
-    let originMethod = Element.prototype[ method ];
-
-    window.Element.prototype[ method ] = function( el, ...args ){
-      if( el && el.nodeName === 'SCRIPT' && el.ownerAppWindow ){
-        injectScriptCallBack( el );
-      }
-
-      try {
-        return originMethod.call( this, el, ...args );
-      } catch {
-        return originMethod.apply( this, [el, ...args] );
-      }
-    }
+    const originMethod = window.Element.prototype[ method ];
+    window.Element.prototype[ method ] = makeElInjector(originMethod);
   }
 }
