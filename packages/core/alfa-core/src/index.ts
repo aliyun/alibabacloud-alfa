96fa8070590cf5406857aca611442c63358f9346
liusi.xbw <liusi.xbw@alibaba-inc.com>
2020-10-29 18:19:21 +0800

    :sparkles: add the alfa core

--- packages/core/alfa-core/src/index.ts
+++ packages/core/alfa-core/src/index.ts
@@ -1,7 +1,32 @@
 import { createMicroApp as createConsoleOSMicroApp } from '@alicloud/console-os-kernal';
 
-const createMicroApp = () => {
-
+export interface IAppManifest {
+  scripts: string[];
+  styles: string[];
 }
 
- export default createMicroApp
+export interface IAppConfig<T = any> {
+  entry: IAppManifest;
+  name: string;
+  container?: HTMLElement;
+  props?: Record<string, T>;
+}
+
+const createMicroApp = <T>(appConfig: IAppConfig<T>) => {
+  return createConsoleOSMicroApp({
+    name: appConfig.name,
+    manifest: {
+      name: appConfig.name,
+      resources: {},
+      entrypoints: {
+        index: {
+          js: appConfig.entry.scripts,
+          css: appConfig.entry.styles,
+        }
+      }
+    }
+  });
+}
+
+ export default createMicroApp;
+ 
