e1289a1528e4ebfe0ecd85dd8cf7228945e137c7
Boelroy <boelroy@live.com>
2021-08-18 16:01:21 +0800

    fix: href error

--- packages/react/react-portal/src/mount.tsx
+++ packages/react/react-portal/src/mount.tsx
@@ -74,7 +74,7 @@ export function mount<T extends EmitterProps>(App: AppComponent<T>, container?: 
       if (target.tagName === 'A' && target.hasAttribute('data-consoleos-external-router')) {
         e.preventDefault();
         e.stopPropagation();
-        emitter && emitter.emit(`${name||id}:external-router`, target.href);
+        emitter && emitter.emit(`${name||id}:external-router`, target.getAttribute('href'));
       }
     }
 
