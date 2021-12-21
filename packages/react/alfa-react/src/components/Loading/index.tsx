48ea397b2435468a88ff64eecd5675e58c41d585
Boelroy <boelroy@live.com>
2021-12-21 22:00:08 +0800

    fix: ssr loading blink problem

--- packages/react/alfa-react/src/components/Loading/index.tsx
+++ packages/react/alfa-react/src/components/Loading/index.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
 import Skeleton from './Skeleton';
 
-export default function getLoading({loading}: {loading?: boolean | React.ReactChild}) {
+const initialPath = window.location.pathname;
+
+interface IProps {
+  loading?: boolean | React.ReactChild;
+  microAppContainer?: string
+}
+
+export default function getLoading({loading, microAppContainer }: IProps) {
+  // 第一次如果是 ssr 出来的内容直接拿 ssr 的内容作为骨架做展示
+  // 防止出现首屏抖动
+  if (microAppContainer) {
+    const node = document.querySelector(microAppContainer);
+    //@ts-ignore
+    if (initialPath === window.location.pathname && window.__isSSR) {
+      return <div dangerouslySetInnerHTML={{ __html: node ? node.innerHTML : '' }} />;
+    }
+  }
+
   if (loading === false) {
     return null;
   } else if (loading && React.isValidElement(loading)) {
