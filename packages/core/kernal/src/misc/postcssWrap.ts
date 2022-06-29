9586c8b44153dc2aa06edcfea01ab4cea2290e6e
栗知 <yr247308@alibaba-inc.com>
2022-06-29 11:16:53 +0800

    chore

--- packages/core/kernal/src/misc/postcssWrap.ts
+++ packages/core/kernal/src/misc/postcssWrap.ts
@@ -15,10 +15,10 @@ interface IOptions {
 }
 
 function increaseSpecifityOfRule(rule: postcss.Rule, opts: IOptions) {
-  rule.selectors = rule.selectors.map(function(selector: string) {
+  rule.selectors = rule.selectors.map((selector: string) => {
     // Apply it to the selector itself if the selector is a `root` level component
     // `html:not(#\\9):not(#\\9):not(#\\9)`
-    if(
+    if (
       selector === 'html' ||
       selector === ':root' ||
       selector === ':host' ||
@@ -29,17 +29,17 @@ function increaseSpecifityOfRule(rule: postcss.Rule, opts: IOptions) {
 
     // Otherwise just make it a descendant (this is what will happen most of the time)
     // `:not(#\\9):not(#\\9):not(#\\9) .foo`
-    return opts.stackableRoot.repeat(opts.repeat) + ' ' + selector;
+    return `${opts.stackableRoot.repeat(opts.repeat) } ${ selector}`;
   });
 
-  if(opts.overrideIds) {
-    if(
+  if (opts.overrideIds) {
+    if (
       // If an id is in there somewhere
-      (new RegExp('#(?!' + escapeStringRegexp(CSS_ESCAPED_TAB) + ')')).test(rule.selector) ||
+      (new RegExp(`#(?!${ escapeStringRegexp(CSS_ESCAPED_TAB) })`)).test(rule.selector) ||
       // Or it is an attribute selector with an id
       (/\[id/).test(rule.selector)
     ) {
-      rule.walkDecls(function(decl) {
+      rule.walkDecls((decl) => {
         decl.important = true;
       });
     }
@@ -52,21 +52,20 @@ const defaultOptions = {
   // Whether to add !important to declarations in rules with id selectors
   overrideIds: true,
   // The thing we repeat over and over to make up the piece that increases specificity
-  stackableRoot: ':not(#' + CSS_ESCAPED_TAB + ')'
+  stackableRoot: `:not(#${ CSS_ESCAPED_TAB })`,
 };
 
 // Plugin that adds `:not(#\\9)` selectors to the front of the rule thus increasing specificity
-export const postcssWrap = postcss.plugin('postcss-css-wrapper', function(options: IOptions | undefined) {
-
+export const postcssWrap = postcss.plugin('postcss-css-wrapper', (options: IOptions | undefined) => {
   const opts = objectAssign({}, defaultOptions, options);
 
-  return function(css: postcss.Root) {
-    css.walkRules(function(rule: postcss.Rule) {
+  return function (css: postcss.Root) {
+    css.walkRules((rule: postcss.Rule) => {
       // Avoid adding additional selectors (stackableRoot) to descendant rules of @keyframe {}
       // i.e. `from`, `to`, or `{number}%`
-      var isInsideKeyframes = rule.parent.type === 'atrule' && rule.parent.name === 'keyframes';
+      const isInsideKeyframes = rule.parent.type === 'atrule' && rule.parent.name === 'keyframes';
 
-      if(!isInsideKeyframes) {
+      if (!isInsideKeyframes) {
         increaseSpecifityOfRule(rule, opts);
       }
     });
