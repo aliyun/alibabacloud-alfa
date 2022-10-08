13d6a14445c94f0ca128ade77976517e77617257
栗知 <yr247308@alibaba-inc.com>
2022-10-08 15:07:23 +0800

    fix: inject windMessages to sandbox

--- packages/react/alfa-react/src/hooks/beforeLoadHook.ts
+++ packages/react/alfa-react/src/hooks/beforeLoadHook.ts
@@ -3,6 +3,10 @@ import { IAppConfig, getConfig, getI18nMessages, IWin } from '@alicloud/alfa-cor
 import { getConsoleConfig } from '../utils/getConsoleConfig';
 import { getConsoleGlobal } from '../utils/getConsoleGlobal';
 
+interface IAliyunWin extends IWin {
+  ALIYUN_WIND_MESSAGE?: Partial<Record<string, string>>;
+}
+
 // inject consoleConfig & locales after load
 async function afterLoadHook(appConfig: IAppConfig) {
   const { app, logger, sandbox } = appConfig;
@@ -24,16 +28,17 @@ async function afterLoadHook(appConfig: IAppConfig) {
   const CONFIG_END_TIME = Date.now();
 
   const i18nMessages = {
-    ...(window as IWin).ALIYUN_CONSOLE_I18N_MESSAGE,
+    ...(window as IAliyunWin).ALIYUN_CONSOLE_I18N_MESSAGE,
     ...messages,
   };
 
 
   // inject global variables when sandbox is valid
   if (app?.context && !sandbox?.disable) {
-    (app.context.window as IWin).ALIYUN_CONSOLE_CONFIG = consoleConfig;
-    (app.context.window as IWin).ALIYUN_CONSOLE_GLOBAL = consoleGlobal;
-    (app.context.window as IWin).ALIYUN_CONSOLE_I18N_MESSAGE = i18nMessages;
+    (app.context.window as IAliyunWin).ALIYUN_CONSOLE_CONFIG = consoleConfig;
+    (app.context.window as IAliyunWin).ALIYUN_CONSOLE_GLOBAL = consoleGlobal;
+    (app.context.window as IAliyunWin).ALIYUN_CONSOLE_I18N_MESSAGE = i18nMessages;
+    (app.context.window as IAliyunWin).ALIYUN_WIND_MESSAGE = (window as IAliyunWin).ALIYUN_WIND_MESSAGE;
   }
 
   const END_TIME = Date.now();
