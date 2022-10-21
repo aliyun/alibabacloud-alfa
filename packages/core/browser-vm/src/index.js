c16b5ddab0c4bbd2bbd1ab0a46aa89ebf465e53a
栗知 <yr247308@alibaba-inc.com>
2022-10-21 17:33:41 +0800

    chore: remove element inject for consoleBase

--- packages/core/browser-vm/src/index.js
+++ packages/core/browser-vm/src/index.js
@@ -5,19 +5,19 @@
  * @createAt 2019085
  */
 
-import './Elements';
+// import './Elements';
 import Context from './Context';
 
-export const createContext = async ( conf = {} ) => {
-  return await Context.create( conf );
-}
+export const createContext = async (conf = {}) => {
+  return await Context.create(conf);
+};
 
-export const removeContext = async ( context ) => {
-  return await Context.remove( context );
-}
+export const removeContext = async (context) => {
+  return await Context.remove(context);
+};
 
 export const evalScripts = async (code, conf = {}) => {
-  const ctx = await Context.create( conf );
+  const ctx = await Context.create(conf);
   const resolver = new Function(`
     return function({window, location, history, document}){ 
       with(window.__CONSOLE_OS_GLOBAL_VARS_) { 
@@ -25,4 +25,4 @@ export const evalScripts = async (code, conf = {}) => {
       }
     }//@sourceURL=${conf.name}`);
   return resolver({ ...ctx });
-}
+};
