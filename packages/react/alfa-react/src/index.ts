bcadf3bc757e150f10b70f570727cfdb079d5220
yr247308 <yr247308@alibaba-inc.com>
2023-11-28 15:11:52 +0800

    fix: export utils from alfa-react

--- packages/react/alfa-react/src/index.ts
+++ packages/react/alfa-react/src/index.ts
@@ -3,5 +3,6 @@ export { default as createAlfaApp, useAlfaApp } from './createAlfaApp';
 export { default as createAlfaWidget, useAlfaWidget } from './createAlfaWidget';
 export { eventEmitter as widgetEventEmitter } from './widget/index';
 export { default as addGlobalRequestInterceptor } from './addGlobalRequestInterceptor';
+export * from './utils';
 // todo
 export { createEventBus, prefetch } from '@alicloud/alfa-core';
