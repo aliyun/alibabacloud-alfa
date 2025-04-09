29c04b082c3753a11d754c773416e8897d319a30
yr247308 <yr247308@alibaba-inc.com>
2025-04-09 21:10:13 +0800

    fix(build): fix get correct webpack chunk in sandbox

--- packages/next/plugin-webpack5-os/src/OSJsonpPlugin.ts
+++ packages/next/plugin-webpack5-os/src/OSJsonpPlugin.ts
@@ -75,12 +75,12 @@ export class OSJsonpWebpackPlugin {
   }
 
   private _wrapCodeWithOSJsonp(id: string) {
-    const injectVars = ['window', 'location', 'history', 'document', ...(this.option.injectVars || [])];
+    const injectVars = ['window', 'location', 'history', 'document', 'self', ...(this.option.injectVars || [])];
     const jsonpCall = this.option.jsonpCall || 'window.__CONSOLE_OS_GLOBAL_HOOK__';
 
     return [`
 if (!window.__CONSOLE_OS_GLOBAL_HOOK__){window.__CONSOLE_OS_GLOBAL_VARS_={};window.__CONSOLE_OS_GLOBAL_HOOK__ = function(id, resolver) {resolver(undefined, undefined, undefined, {${injectVars.map((item) => `${item}: ${item}`).join(',')}})};window.__CONSOLE_OS_GLOBAL_HOOK__.standalone = true}
-${jsonpCall}(${JSON.stringify(id)}, function(require, module, exports, context){ ${injectVars.map((item) => `var ${item} = context.${item}`).join(';')};with(window.__CONSOLE_OS_GLOBAL_VARS_) { \n
+${jsonpCall}(${JSON.stringify(id)}, function(require, module, exports, context){ ${injectVars.map((item) => `var ${item} = context.${item === 'self' ? 'window' : item}`).join(';')};with(window.__CONSOLE_OS_GLOBAL_VARS_) { \n
 `, '\n}})'];
   }
 }
